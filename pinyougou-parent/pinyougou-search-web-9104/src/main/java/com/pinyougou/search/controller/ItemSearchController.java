cbd0a8a1cdc1ad727cfecb41c9bbf0f339fc56fb
love550185890 <lsf_320@126.com>
2019-10-01 22:04:04 +0800

    增加search服务的web层

---
+++ pinyougou-parent/pinyougou-search-web-9104/src/main/java/com/pinyougou/search/controller/ItemSearchController.java
@@ -0,0 +1,27 @@
+package com.pinyougou.search.controller;
+
+import com.pinyougou.search.service.ItemSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @Description
+ * @Author
+ * @Date
+ */
+@RestController
+@RequestMapping(value = "/itemSearch")
+public class ItemSearchController {
+
+    @Autowired
+    ItemSearchService itemSearchService;
+
+    @RequestMapping("/search")
+    public Map<String,Object> search(@RequestBody Map searchMap){
+        return itemSearchService.search(searchMap);
+    }
+}
