0fc24221b370cb093c831686531ada11c4aeca75
love550185890 <lsf_320@126.com>
2019-10-03 00:55:34 +0800

    angularjs添加信任html过滤器

--- pinyougou-parent/pinyougou-search-web-9104/src/main/webapp/js/base.js
+++ pinyougou-parent/pinyougou-search-web-9104/src/main/webapp/js/base.js
@@ -1,1 +1,7 @@
 var app = angular.module("pinyougou", []);
+//angularjs添加信任html
+app.filter("trustAsHtml",["$sce",function ($sce) {
+    return function (data) {
+      return $sce.trustAsHtml(data);
+    };
+}]);
