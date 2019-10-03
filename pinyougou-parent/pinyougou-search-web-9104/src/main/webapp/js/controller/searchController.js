f164a76129c7d8a0f565c14212fc42efefaaf3c7
love550185890 <lsf_320@126.com>
2019-10-03 08:16:36 +0800

    接收传递过来的搜索关键字

--- pinyougou-parent/pinyougou-search-web-9104/src/main/webapp/js/controller/searchController.js
+++ pinyougou-parent/pinyougou-search-web-9104/src/main/webapp/js/controller/searchController.js
@@ -1,4 +1,4 @@
-app.controller('searchController',function($scope,searchService){
+app.controller('searchController',function($scope,$location,searchService){
     //定义搜索变量
     $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':'','pageNo':1,'pageSize':40,'sortField':'','sort':'' };//搜索对象
     //搜索
@@ -104,4 +104,11 @@ app.controller('searchController',function($scope,searchService){
         }
         return false;
     }
+
+    //加载查询字符串
+    $scope.loadkeywords=function(){
+        $scope.searchMap.keywords=  $location.search()['keywords'];
+        $scope.search();
+    }
+
 });
